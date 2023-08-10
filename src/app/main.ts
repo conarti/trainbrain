4ddc3411902d252feaf073c4a826d1b31695ceda
conarti <abelous2009@gmail.com>
2023-08-10 19:18:53 +0300

    feat: setup quasar ui

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,18 @@
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
 import { createApp } from 'vue';
 import App from './app.vue';
 import router from './router';
 
 const app = createApp(App);
 
+app.use(Quasar, {
+  config: {
+    dark: true,
+  },
+});
 app.use(createPinia());
 app.use(router);
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import {
   fileURLToPath,
   URL,
 } from 'node:url';
+import {
+  quasar,
+  transformAssetUrls,
+} from '@quasar/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      autoImportComponentCase: 'combined',
+      sassVariables: 'src/app/quasar-variables.scss',
+    }),
   ],
   resolve: {
     alias: {
