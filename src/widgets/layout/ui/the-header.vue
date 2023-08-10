816d1d2f4d0db63e3a5a94baa56f99dffeec053e
conarti <abelous2009@gmail.com>
2023-08-10 19:51:33 +0300

    fix(layout): use 'q-toolbar-title' for app title, remove 'App' from title

--- src/widgets/layout/ui/the-header.vue
+++ src/widgets/layout/ui/the-header.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import { RouteNames } from '@/shared/config/route-names';
 
-const APP_TITLE = 'TrainBrain App';
+const APP_TITLE = 'TrainBrain';
 </script>
 
 <template>
   <q-toolbar>
-    <q-btn
-      flat
-      :label="APP_TITLE"
-      disable
-    />
+    <q-toolbar-title>
+      {{ APP_TITLE }}
+    </q-toolbar-title>
 
     <q-space />
 
