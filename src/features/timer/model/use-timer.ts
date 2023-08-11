32408f95b40cc35da650dadc295f75651202ce6d
conarti <abelous2009@gmail.com>
2023-08-11 11:05:15 +0300

    feat: setup timer feature

---
+++ src/features/timer/model/use-timer.ts
@@ -0,0 +1,54 @@
+import {
+  computed,
+  ref,
+} from 'vue';
+
+type TimeInSeconds = number;
+
+const TIMER_UPDATE_TIME_IN_MS = 1000;
+
+function withZero(time: number): string {
+  return time.toString().padStart(2, '0');
+}
+
+function formatTime(timeInSeconds: number): string {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;;
+  return `${withZero(minutes)}:${withZero(seconds)}`;
+}
+
+export function useTimer() {
+  const time = ref<TimeInSeconds>(0);
+  const formattedTime = computed(() => formatTime(time.value));
+
+  const intervalId = ref<ReturnType<typeof setInterval>>();
+
+  function incrementByOneSecond() {
+    time.value += 1;
+  }
+
+  function start() {
+    intervalId.value = setInterval(incrementByOneSecond, TIMER_UPDATE_TIME_IN_MS);
+  }
+  function stop() {
+    clearInterval(intervalId.value);
+    intervalId.value = undefined;
+  }
+  function reset() {
+    time.value = 0;
+  }
+  function restart() {
+    stop();
+    reset();
+    start();
+  }
+
+  return {
+    time,
+    formattedTime,
+    start,
+    stop,
+    reset,
+    restart,
+  };
+}
