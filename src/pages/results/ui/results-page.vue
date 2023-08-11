16ed7499cb019db50c788dc415933c7a986f6503
conarti <abelous2009@gmail.com>
2023-08-11 21:34:20 +0300

    feat(results-page): setup layout

--- src/pages/results/ui/results-page.vue
+++ src/pages/results/ui/results-page.vue
@@ -1,26 +1,98 @@
 <script setup lang="ts">
+import groupBy from 'lodash/groupBy';
+import { date } from 'quasar';
 import { computed } from 'vue';
-import { useUserGamesResults } from '@/features/user-games-results';
+import type { ExerciseWithSolution } from '@/widgets/math-game';
+import { formatTime } from '@/features/timer';
+import {
+  type MathGameResult,
+  useUserGamesResults,
+} from '@/features/user-games-results';
+
+const DATE_WITH_TIME_UI_FORMAT = 'D MMM YYYY / HH:mm';
+const DATE_UI_FORMAT = 'D MMM YYYY';
 
 const { results } = useUserGamesResults();
 
+const resultsGroupedByDay = computed(() => {
+  return groupBy(results.value, (mathGameResult: MathGameResult) => {
+    return date.formatDate(mathGameResult.date, DATE_UI_FORMAT);
+  });
+});
+
 const hasNoResults = computed(() => results.value.length === 0);
+
+/* @duplicate show-train-results + exercise-solutions */
+function isInvalidResult(exerciseWithSolution: ExerciseWithSolution) {
+  return exerciseWithSolution.solution !== exerciseWithSolution.result;
+}
+
+/* @duplicate show-train-results */
+function getMistakeSolutionsCount(solutions: ExerciseWithSolution[]): number {
+  return solutions.filter(isInvalidResult).length;
+}
+function getMistakeSolutionsPercent(mistakeSolutionsCount: number, totalSolutionsCount: number): number {
+  return Math.floor(mistakeSolutionsCount / totalSolutionsCount * 100);
+}
 </script>
 
 <template>
-  <h5
-    v-if="hasNoResults"
-    class="text-center"
-  >
-    No results
-  </h5>
-  <template v-else>
-    <div
-      v-for="(result, index) in results"
-      :key="index"
+  <q-page padding>
+    <h5
+      v-if="hasNoResults"
+      class="text-center"
     >
-      <h6>{{ result.time }}</h6>
-      <p>{{ result.solutions }}</p>
-    </div>
-  </template>
+      No results
+    </h5>
+    <template v-else>
+      <h6 class="q-ma-sm">
+        Math Game
+      </h6>
+      <q-list
+        bordered
+        padding
+      >
+        <template
+          v-for="(dayResults, day) in resultsGroupedByDay"
+          :key="day"
+        >
+          <q-item-label
+            header
+            caption
+          >
+            {{ day }}
+          </q-item-label>
+
+          <template
+            v-for="(result, index) in dayResults"
+            :key="index"
+          >
+            <q-item>
+              <q-item-section>
+                <q-item-label>
+                  Time: {{ formatTime(result.time) }}
+                </q-item-label>
+                <q-item-label caption>
+                  Mistakes:
+                  <span class="text-red">{{
+                    getMistakeSolutionsCount(result.solutions)
+                  }}</span>/{{ result.solutions.length }} ({{
+                    getMistakeSolutionsPercent(getMistakeSolutionsCount(result.solutions), result.solutions.length)
+                  }}%)
+                </q-item-label>
+              </q-item-section>
+              <q-item-section
+                side
+                top
+              >
+                <q-item-label caption>
+                  {{ date.formatDate(result.date, DATE_WITH_TIME_UI_FORMAT) }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </template>
+        </template>
+      </q-list>
+    </template>
+  </q-page>
 </template>
