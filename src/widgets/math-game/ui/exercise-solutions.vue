66fd1e01932a03efda19f93fe4ab7ddc8bc01e2d
conarti <abelous2009@gmail.com>
2023-08-11 22:24:51 +0300

    feat(math-game): use q-list/q-item at exercise solutions list

--- src/widgets/math-game/ui/exercise-solutions.vue
+++ src/widgets/math-game/ui/exercise-solutions.vue
@@ -28,21 +28,25 @@ function isInvalidResult(exerciseWithSolution: ExerciseWithSolution) {
   >
     <q-card>
       <q-card-section>
-        <div class="fit row wrap justify-start items-center content-center q-gutter-md">
-          <p
+        <q-list dense>
+          <q-item
             v-for="(exerciseWithSolution, index) in solutions"
             :key="exerciseWithSolution.id"
           >
-            <b>{{ index + 1 }}.</b>
-            {{ exerciseWithSolution.label }}
-            <q-badge
-              rounded
-              :color="isInvalidResult(exerciseWithSolution) ? 'red' : 'green'"
-            >
-              {{ exerciseWithSolution.solution }}
-            </q-badge>
-          </p>
-        </div>
+            <q-item-section>
+              <p class="q-ma-none">
+                <b>{{ index + 1 }}.</b>&nbsp;
+                {{ exerciseWithSolution.label }}
+                <q-badge
+                  rounded
+                  :color="isInvalidResult(exerciseWithSolution) ? 'red' : 'green'"
+                >
+                  {{ exerciseWithSolution.solution }}
+                </q-badge>
+              </p>
+            </q-item-section>
+          </q-item>
+        </q-list>
       </q-card-section>
     </q-card>
   </q-expansion-item>
