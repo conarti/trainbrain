9002b17473a7729dc6fd0fbca974b8f888cd36fe
conarti <abelous2009@gmail.com>
2023-08-11 14:18:07 +0300

    feat(math-game): update results layout

--- src/widgets/math-game/ui/show-train-results.vue
+++ src/widgets/math-game/ui/show-train-results.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { EXERCISES_COUNT } from '../config';
 import type { ExerciseWithSolution } from '../model';
 
 interface Props {
@@ -6,33 +8,74 @@ interface Props {
   results: ExerciseWithSolution[]
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+type Emits = (event: 'restart') => void
+
+const emit = defineEmits<Emits>();
 
 function isInvalidResult(exerciseWithSolutin: ExerciseWithSolution) {
   return exerciseWithSolutin.solution !== exerciseWithSolutin.result;
 }
+
+const mistakeSolutionsCount = computed(() => props.results.filter(isInvalidResult).length);
+const mistakeSolutionsPercent = computed(() => Math.floor(mistakeSolutionsCount.value / EXERCISES_COUNT * 100));
 </script>
 
 <template>
   <q-card flat>
     <q-card-section>
-      <h4 class="q-ma-none text-center">
+      <h6 class="q-ma-none text-center">
+        Elapsed time:
+      </h6>
+      <p class="text-center text-h5">
         {{ time }}
-      </h4>
-    </q-card-section>
-    <q-card-section>
-      <p
-        v-for="result in results"
-        :key="result.id"
-      >
-        {{ result.label }}
-        <q-badge
-          rounded
-          :color="isInvalidResult(result) ? 'red' : 'green'"
-        >
-          {{ result.solution }}
-        </q-badge>
+      </p>
+      <h6 class="q-ma-none text-center">
+        Mistakes made:
+      </h6>
+      <p class="text-center text-h5">
+        <span class="text-red">{{ mistakeSolutionsCount }}</span> ({{ mistakeSolutionsPercent }}%)
       </p>
     </q-card-section>
+    <q-card-section>
+      <q-expansion-item
+        label="Full results"
+        class="shadow-1 overflow-hidden bg-blue-grey-1"
+        header-class="text-bold"
+        style="border-radius: 30px"
+      >
+        <q-card>
+          <q-card-section>
+            <div class="fit row wrap justify-start items-center content-center q-gutter-md">
+              <p
+                v-for="(result, index) in results"
+                :key="result.id"
+              >
+                <b>{{ index + 1 }}.</b>
+                {{ result.label }}
+                <q-badge
+                  rounded
+                  :color="isInvalidResult(result) ? 'red' : 'green'"
+                >
+                  {{ result.solution }}
+                </q-badge>
+              </p>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </q-card-section>
+    <q-card-actions
+      align="stretch"
+      vertical
+    >
+      <q-btn
+        label="Try again"
+        push
+        color="primary"
+        @click="emit('restart')"
+      />
+    </q-card-actions>
   </q-card>
 </template>
