a8c9130559cb685b4ec0a16dea2a8a5508a701a5
conarti <abelous2009@gmail.com>
2023-08-12 12:13:05 +0300

    refactor(main-layout): delete 'the-header' component, move content of it to 'main-layout'

--- src/widgets/layout/ui/main-layout.vue
+++ src/widgets/layout/ui/main-layout.vue
@@ -3,14 +3,38 @@ import {
   QLayout,
   QPageContainer,
 } from 'quasar';
-import TheHeader from './the-header.vue';
+import { RouteNames } from '@/shared/config/route-names';
+import { BrandLogo } from '@/shared/ui/brand-logo';
+
+const APP_TITLE = 'TrainBrain';
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-    <TheHeader />
+    <q-header>
+      <q-toolbar>
+        <BrandLogo size="32" />
+        <q-toolbar-title shrink>
+          {{ APP_TITLE }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          :to="{ name: RouteNames.Train }"
+          label="Train"
+        />
+        <q-route-tab
+          :to="{ name: RouteNames.Results }"
+          label="Results"
+        />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
