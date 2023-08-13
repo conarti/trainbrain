38404a2c5978e8f6753b38af93ea301c2f56bf37
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-13 11:15:07 +0300

    feat: add speed-counting-game to train-page

--- src/pages/train/ui/train-page.vue
+++ src/pages/train/ui/train-page.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { MathGame } from '@/widgets/games/math';
+import { SpeedCountingGame } from '@/widgets/games/speed-counting';
 </script>
 
 <template>
   <q-page padding>
     <MathGame />
+    <SpeedCountingGame />
   </q-page>
 </template>
