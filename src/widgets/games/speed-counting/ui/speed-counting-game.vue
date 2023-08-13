bdb77db8a5332737fc25b8175ebc3e07c4a868ec
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-13 11:32:56 +0300

    refactor: extract game-card to 'entities/game'

--- src/widgets/games/speed-counting/ui/speed-counting-game.vue
+++ src/widgets/games/speed-counting/ui/speed-counting-game.vue
@@ -1,45 +1,9 @@
 <script setup lang="ts">
 
+import { GameCard } from '@/entities/game';
 </script>
 
 <template>
-  <q-card
-    flat
-    bordered
-    tag="form"
-  >
-    <q-card-section>
-      <div class="flex flex-center q-mb-sm">
-        <q-icon
-          class="q-mr-sm"
-          size="sm"
-          name="sym_r_123"
-        />
-        <h5 class="q-ma-none text-center">
-          Speed Counting Game
-        </h5>
-      </div>
-      <p class="text-subtitle2 text-center">
-        Count out loud from 1 to 120 as fast as you can.
-      </p>
-      <p class="text-center q-mb-none">
-        Be sure to pronounce each number clearly.
-        <br>
-        Press "DONE!" when you have finished the counting.
-      </p>
-    </q-card-section>
-    <q-card-actions>
-      <q-btn
-        class="col"
-        label="Start"
-        push
-        color="primary"
-        type="submit"
-      />
-    </q-card-actions>
-  </q-card>
+  <GameCard />
 </template>
 
-<style scoped>
-
-</style>
