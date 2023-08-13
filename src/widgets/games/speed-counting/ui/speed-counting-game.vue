1d588c7c7f826146362c215758bebb8850a37195
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-13 17:56:11 +0300

    feat: add progress and finish game interface

--- src/widgets/games/speed-counting/ui/speed-counting-game.vue
+++ src/widgets/games/speed-counting/ui/speed-counting-game.vue
@@ -1,18 +1,40 @@
 <script setup lang="ts">
 import {
+  formatTime,
+  useStopwatch,
+} from '@/features/stopwatch';
+import {
   GameCard,
   useGameStore,
 } from '@/entities/game';
 
-const { start } = useGameStore();
+const gameStore = useGameStore();
+const {
+  time: gameTime,
+  start: startStopwatch,
+  stop: stopStopwatch,
+  reset: resetStopwatch,
+} = useStopwatch();
+
+function handleStartGame() {
+  gameStore.start();
+  resetStopwatch();
+  startStopwatch();
+}
+
+function handleFinishGame() {
+  gameStore.finish();
+  stopStopwatch();
+}
 </script>
 
 <template>
   <GameCard
+    v-if="gameStore.isGameNotStarted"
     title="Speed Counting Game"
     description="Count out loud from 1 to 120 as fast as you can."
     icon="sym_r_123"
-    @start="start"
+    @start="handleStartGame"
   >
     <template #additional-info>
       <p class="text-center q-mb-none">
@@ -22,4 +44,44 @@ const { start } = useGameStore();
       </p>
     </template>
   </GameCard>
+  <q-card
+    v-else-if="gameStore.isGameInProgress"
+    flat
+    bordered
+  >
+    <q-card-section>
+      <h4 class="q-ma-none q-mb-md text-center">
+        {{ formatTime(gameTime) }}
+      </h4>
+    </q-card-section>
+    <q-card-actions>
+      <q-btn
+        class="col"
+        label="Done"
+        push
+        color="primary"
+        @click="handleFinishGame"
+      />
+    </q-card-actions>
+  </q-card>
+  <q-card
+    v-else-if="gameStore.isGameFinished"
+    flat
+    bordered
+  >
+    <q-card-section>
+      <h6 class="q-ma-none q-mb-md text-center">
+        Your time is {{ formatTime(gameTime) }}
+      </h6>
+    </q-card-section>
+    <q-card-actions>
+      <q-btn
+        class="col"
+        label="Try again"
+        push
+        color="primary"
+        @click="handleStartGame"
+      />
+    </q-card-actions>
+  </q-card>
 </template>
