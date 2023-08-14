9df9a0571ce637da36210490685a4b95dda87966
conarti <abelous2009@gmail.com>
2023-08-14 13:19:28 +0300

    refactor(user-stats): extract local storage adapters

--- src/features/user-stats/model/use-local-storage-results.ts
+++ src/features/user-stats/model/use-local-storage-results.ts
@@ -12,40 +12,54 @@ import type {
 
 const LOCALSTORAGE_RESULTS_KEY = 'trainbrain-results';
 
+type LocalStorageSavedGames = Partial<SavedGames> | null;
+
+function toSavedGames(localStorageSavedGames: LocalStorageSavedGames): SavedGames {
+  const emptySavedGames = new EmptySavedGames();
+
+  if (isNil(localStorageSavedGames)) {
+    return emptySavedGames;
+  }
+
+  function sortMathResults(results: MathGameResult[]): MathGameResult[] {
+    return sortBy(results, (result) => -result.date);
+  }
+
+  /* need to replace undefined after adding new games to SavedGames */
+  const withoutUndefinedResults: SavedGames = { // TODO use function
+    math: localStorageSavedGames.math || emptySavedGames.math,
+    speedCounting: localStorageSavedGames.speedCounting || emptySavedGames.speedCounting,
+  };
+
+  /* sorted by date descending (first is the closest date) */
+  const withSortedMathGame: SavedGames = {
+    ...withoutUndefinedResults,
+    math: sortMathResults(withoutUndefinedResults.math),
+  };
+
+  return withSortedMathGame;
+}
+
+function toLocalStorageSavedGames<T extends SavedGameName>(
+  alreadySavedGames: SavedGames,
+  game: T,
+  result: SavedGameResult<T>,
+): LocalStorageSavedGames {
+  return {
+    ...alreadySavedGames,
+    [game]: [...alreadySavedGames[game], result],
+  };
+}
+
 export function useLocalStorageResults(): StorageStrategy {
   async function get(): Promise<SavedGames> {
-    const savedResults = LocalStorage.getItem<Partial<SavedGames>>(LOCALSTORAGE_RESULTS_KEY);
-
-    const emptySavedGames = new EmptySavedGames();
-
-    if (isNil(savedResults)) {
-      return emptySavedGames;
-    }
-
-    function sortMathResults(results: MathGameResult[]): MathGameResult[] {
-      return sortBy(results, (result) => -result.date);
-    }
-
-    /* need to replace undefined after adding new games to SavedGames */
-    const withoutUndefinedResults: SavedGames = { // TODO use function
-      math: savedResults.math || emptySavedGames.math,
-      speedCounting: savedResults.speedCounting || emptySavedGames.speedCounting,
-    };
-
-    /* sorted by date descending (first is the closest date) */
-    const withSortedMathGame: SavedGames = {
-      ...withoutUndefinedResults,
-      math: sortMathResults(withoutUndefinedResults.math),
-    };
-    return withSortedMathGame;
+    const savedResults = LocalStorage.getItem<Exclude<LocalStorageSavedGames, null>>(LOCALSTORAGE_RESULTS_KEY);
+    return toSavedGames(savedResults);
   }
 
   async function save<T extends SavedGameName>(game: T, result: SavedGameResult<T>) {
-    const savedResults = await get();
-    const updatedSavedGames: SavedGames = {
-      ...savedResults,
-      [game]: [...savedResults[game], result],
-    };
+    const alreadySavedGames = await get();
+    const updatedSavedGames = toLocalStorageSavedGames(alreadySavedGames, game, result);
     LocalStorage.set(LOCALSTORAGE_RESULTS_KEY, updatedSavedGames);
   }
 
