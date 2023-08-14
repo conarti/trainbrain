792ceeaaa00da746372e277d26ba86e154ba0598
conarti <abelous2009@gmail.com>
2023-08-14 14:01:19 +0300

    style(user-stats): inline reexport from one source

--- src/features/user-stats/model/index.ts
+++ src/features/user-stats/model/index.ts
@@ -1,3 +1,5 @@
 export { useUserStats } from './use-user-stats';
-export type { MathGameResult } from './types';
-export type { SpeedCountingGameResult } from './types';
+export type {
+  MathGameResult,
+  SpeedCountingGameResult, 
+} from './types';
