cba990b61e2a06310002cdb9662f92ff1f9534ee
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-14 22:02:33 +0300

    feat(routes): add beforeEnter for game page

--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -1,7 +1,12 @@
+import type {
+  NavigationGuardNext,
+  RouteLocationNormalized,
+} from 'vue-router';
 import { ResultsPage } from '@/pages/results';
 import { TheGamePage } from '@/pages/the-game';
 import { TrainPage } from '@/pages/train';
 import { MainLayout } from '@/widgets/layout';
+import { GAME_NAMES } from '@/entities/game';
 import { RouteNames } from '@/shared/config/route-names';
 
 export const routes = [
@@ -23,6 +28,13 @@ export const routes = [
         path: 'games/:name',
         name: RouteNames.Game,
         component: TheGamePage,
+        beforeEnter: (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+          const paramName = to.params?.['name']?.toString() ?? '';
+          if (!Object.values(GAME_NAMES).includes(paramName)) {
+            return next({ name: RouteNames.Train });
+          }
+          return next();
+        },
       },
     ],
   },
