2cb58bb3b23395f387cfb31896fe73d828ea2f82
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-18 12:19:51 +0300

    feat(games-page): add game-cards

--- src/pages/games/ui/games-page.vue
+++ src/pages/games/ui/games-page.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
+import {
+  GAME_NAMES,
+  GameCard,
+} from '@/entities/game';
 </script>
 
 <template>
   <q-page
-    class="q-gutter-sm"
+    class="row flex-center q-gutter-md"
     padding
   >
-    <router-link to="/games/math">
-      <q-card>math</q-card>
-    </router-link>
-    <router-link to="/games/speed-counting">
-      <q-card>speed-counting</q-card>
-    </router-link>
+    <GameCard
+      class="col-10 col-sm"
+      :route-name="GAME_NAMES.MATH"
+      label="Math"
+      icon="sym_r_calculate"
+    />
+    <GameCard
+      class="col-10 col-sm"
+      :route-name="GAME_NAMES.SPEED_COUNTING"
+      label="Speed Counting"
+      icon="sym_r_record_voice_over"
+    />
   </q-page>
 </template>
