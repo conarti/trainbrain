22469a8b9e352faef8d2a8a0125b71d8e16ddcbb
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-25 12:44:12 +0300

    refactor: remove unused GAME_STATUS enum

--- src/entities/game/model/types.ts
+++ src/entities/game/model/types.ts
@@ -1,10 +1,3 @@
-export enum GAME_STATUS {
-  NOT_STARTED = 'not-started',
-  IN_PROGRESS = 'in-progress',
-  PAUSED = 'paused',
-  FINISHED = 'finished',
-}
-
 export enum GAME_NAMES {
   MATH = 'math',
   SPEED_COUNTING = 'speed-counting',
