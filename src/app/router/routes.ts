347ddbcd02cae5867ca5e231e637d63679c88784
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-25 11:56:52 +0300

    refactor: remove the-game page

--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -1,6 +1,5 @@
 import { GamesPage } from '@/pages/games';
 import { ResultsPage } from '@/pages/results';
-import { TheGamePage } from '@/pages/the-game';
 import { TrainPage } from '@/pages/train';
 import { MainLayout } from '@/widgets/layout';
 import { RouteNames } from '@/shared/config/route-names';
@@ -28,12 +27,6 @@ export const routes = [
             path: '',
             name: RouteNames.Games,
             component: GamesPage,
-          },
-          {
-            path: ':name',
-            name: RouteNames.Game,
-            component: TheGamePage,
-            beforeEnter: [checkGameName],
           },
         ],
       },
--- src/pages/the-game/ui/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as TheGamePage } from './the-game-page.vue';
