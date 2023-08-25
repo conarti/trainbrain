f85edf8af9ec306bb401dd3321bbccb1f3aff3dc
Diana Ivanova <diana.ivanova1998@gmail.com>
2023-08-25 17:05:45 +0300

    refactor: extract start-game.vue

---
+++ src/widgets/games/speed-counting/ui/start-game.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { StartGameCard } from '@/entities/game';
+type Emits = (event: 'start') => void;
+const emit = defineEmits<Emits>();
+
+function start() {
+  emit('start');
+}
+</script>
+
+<template>
+  <StartGameCard
+    title="Speed Counting Game"
+    description="Count out loud from 1 to 120 as fast as you can."
+    icon="sym_r_record_voice_over"
+    @start="start"
+  >
+    <template #additional-info>
+      <p class="text-center q-mb-none">
+        Be sure to pronounce each number clearly.
+        <br>
+        Press "DONE!" when you have finished the counting.
+      </p>
+    </template>
+  </StartGameCard>
+</template>
