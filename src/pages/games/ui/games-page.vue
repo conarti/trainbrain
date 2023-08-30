dd6002fe6f02f0698dc6f57efd286f018ba5f70a
conarti <abelous2009@gmail.com>
2023-08-30 23:45:33 +0300

    fix(games-page): broken layout paddings

--- src/pages/games/ui/games-page.vue
+++ src/pages/games/ui/games-page.vue
@@ -4,20 +4,19 @@ import { GAME_CARDS_LIST } from '../config';
 </script>
 
 <template>
-  <q-page
-    class="row justify-center content-start q-gutter-md q-pa-md"
-    padding
-  >
-    <div
-      v-for="(card, idx) in GAME_CARDS_LIST"
-      :key="idx"
-      class="col-all"
-    >
-      <GameLink
-        :route-name="card.routeName"
-        :label="card.label"
-        :icon="card.icon"
-      />
+  <q-page padding>
+    <div class="row justify-center content-start q-col-gutter-sm">
+      <div
+        v-for="(card, idx) in GAME_CARDS_LIST"
+        :key="idx"
+        class="col-all"
+      >
+        <GameLink
+          :route-name="card.routeName"
+          :label="card.label"
+          :icon="card.icon"
+        />
+      </div>
     </div>
   </q-page>
 </template>
