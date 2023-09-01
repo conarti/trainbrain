e138854114cbb692c70c4e1a632d724b2038211a
conarti <abelous2009@gmail.com>
2023-09-01 12:31:02 +0300

    refactor(results-page): extract 'math-game-results' component

--- src/pages/results/ui/results-page.vue
+++ src/pages/results/ui/results-page.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import isEmpty from 'lodash/isEmpty';
 import { computed } from 'vue';
-import {
-  getMistakeSolutionsCount,
-  getMistakeSolutionsPercent,
-} from '@/widgets/games/math';
 import { useSavedGames } from '@/features/saved-games';
 import { formatTime } from '@/features/stopwatch';
 import EmptyResults from './empty-results.vue';
 import GameResults from './game-results.vue';
+import MathGameResults from './math-game-results.vue';
 
 const { savedGames } = useSavedGames();
 
@@ -21,22 +18,7 @@ const hasNoResults = computed(() => isEmpty(mathResults) && isEmpty(speedCountin
   <q-page padding>
     <EmptyResults v-if="hasNoResults" />
 
-    <game-results
-      title="Math Game"
-      :results="mathResults"
-    >
-      <template #result-body="{ value }">
-        <q-item-label>
-          Time: {{ formatTime(value.time) }}
-        </q-item-label>
-        <q-item-label caption>
-          Mistakes:
-          <span class="text-red">{{
-            getMistakeSolutionsCount(value.solutions)
-          }}</span>/{{ value.solutions.length }} ({{ getMistakeSolutionsPercent(value.solutions) }}%)
-        </q-item-label>
-      </template>
-    </game-results>
+    <MathGameResults :results="mathResults" />
 
     <game-results
       title="Speed Counting Game"
