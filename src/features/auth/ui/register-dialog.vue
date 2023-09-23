4249e09fbca5c29d5263e4df27dc3c5244fe9863
conarti <abelous2009@gmail.com>
2023-09-23 17:38:07 +0300

    feat(auth): add 'register-form' layout

--- src/features/auth/ui/register-dialog.vue
+++ src/features/auth/ui/register-dialog.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { FullscreenDialog } from '@/shared/ui/fullscreen-dialog';
+import RegisterForm from './register-form.vue';
 </script>
 
 <template>
   <fullscreen-dialog title="Register">
-    register dialog
+    <q-card-section>
+      <register-form />
+    </q-card-section>
   </fullscreen-dialog>
 </template>
---
+++ src/features/auth/ui/register-form.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { VALIDATION_RULES } from '@/shared/constants/validation-rules';
+import InputPassword from './input-password.vue';
+import InputUsername from './input-username.vue';
+
+const firstname = ref<string>('');
+const lastname = ref<string>('');
+const username = ref<string>('');
+const password = ref<string>('');
+
+function handleSubmit() {
+  // TODO
+}
+</script>
+
+<template>
+  <q-form
+    class="column q-gutter-y-md"
+    @submit="handleSubmit"
+  >
+    <q-input
+      v-model="firstname"
+      label="Firstname"
+      lazy-rules
+      :rules="[VALIDATION_RULES.required]"
+    />
+    <q-input
+      v-model="lastname"
+      label="Lastname"
+      lazy-rules
+      :rules="[VALIDATION_RULES.required]"
+    />
+    <InputUsername v-model="username" />
+    <InputPassword v-model="password" />
+    <q-btn
+      type="submit"
+      push
+      color="primary"
+      padding="sm md"
+      rounded
+    >
+      Sign Up
+    </q-btn>
+  </q-form>
+</template>
