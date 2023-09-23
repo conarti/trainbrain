fb228a84893506a972d404df6130b9f679ceddec
conarti <abelous2009@gmail.com>
2023-09-23 17:37:51 +0300

    feat(validation-rules): add 'required' rule

--- src/shared/constants/validation-rules.ts
+++ src/shared/constants/validation-rules.ts
@@ -1,13 +1,16 @@
+import isEmpty from 'lodash/isEmpty';
 import isNumber from 'lodash/isNumber';
 import isString from 'lodash/isString';
 import type { ValidationRule } from 'quasar';
 
 type ValidationRuleName = 'number'
   | 'username'
-  | 'password';
+  | 'password'
+  | 'required';
 
 export const VALIDATION_RULES: Record<ValidationRuleName, Exclude<ValidationRule<unknown>, 'EmbeddedValidationRule'>> = {
   number: (value) => isNumber(value) || 'Please write valid number',
   username: (value) => isString(value) && value.length > 4 || 'Username must have at least 5 characters',
   password: (value) => isString(value) && value.length > 6 || 'Password must have at least 7 characters',
+  required: (value) => !isEmpty(value) || 'This field is required',
 };
