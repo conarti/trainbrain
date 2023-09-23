da4911189da315396bf97a99389a1635ee5bb378
conarti <abelous2009@gmail.com>
2023-09-23 17:26:16 +0300

    feat(validation-rules): add 'username' and 'password' rules

--- src/shared/constants/validation-rules.ts
+++ src/shared/constants/validation-rules.ts
@@ -1,8 +1,13 @@
 import isNumber from 'lodash/isNumber';
+import isString from 'lodash/isString';
 import type { ValidationRule } from 'quasar';
 
-type ValidationRuleName = 'number';
+type ValidationRuleName = 'number'
+  | 'username'
+  | 'password';
 
 export const VALIDATION_RULES: Record<ValidationRuleName, Exclude<ValidationRule<unknown>, 'EmbeddedValidationRule'>> = {
   number: (value) => isNumber(value) || 'Please write valid number',
+  username: (value) => isString(value) && value.length > 4 || 'Username must have at least 5 characters',
+  password: (value) => isString(value) && value.length > 6 || 'Password must have at least 7 characters',
 };
